77fa368b340c70d861a678bef9d7341c14a8a3b9
Osmi Otalora <ootalorague@uniminuto.edu.co>
2019-05-20 11:33:50 -0500

    agregando controlador

---
+++ controller/session/coordinator.php
@@ -0,0 +1,12 @@
+<?php
+  session_start();
+  
+  $actor;
+  if (condition) {
+    // code...
+  } else {
+    // code...
+  }
+
+  require_once '../../view/session/login.php';
+?>
---
+++ controller/session/seller.php
@@ -0,0 +1,13 @@
+<?php
+  session_start();
+
+  $actor;
+  if (condition) {
+    // code...
+  } else {
+    // code...
+  }
+
+
+  require_once '../../view/session/login.php';
+?>
